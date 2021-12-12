bb2c6855f64c4f7ed95eb5dba192ce7c18ce9158
Gabriel Rodrigues <GABRIEL.RTLIMA@UPE.BR>
2021-12-12 02:10:25 -0300

    tipos de declaracao

---
+++ funcoes/tiposDeclaracao.js
@@ -0,0 +1,20 @@
+console.log(soma(3, 4))
+// sub(2,3) chamar só depois que declarar
+// mult(2,3) chamar so depois q declarar
+
+
+// function declaration
+function soma() {
+    return x + y
+}
+
+// function expression
+const sub = function(x,y) {
+    return x - y
+}
+
+// named function expression
+const mult = function mult(x, y) {
+    return x * y
+}
+
