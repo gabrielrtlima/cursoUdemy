b72b605bd0c2cfff53245b0b746c2cdfbf251f77
Gabriel Rodrigues <GABRIEL.RTLIMA@UPE.BR>
2021-12-14 06:07:44 -0300

    iniciando estudos/revisão de POO com javascript

---
+++ objeto/oo.js
@@ -0,0 +1,22 @@
+// codigo não executavel!
+
+// Procedural
+processamento(valor1, valor2, valor3)
+
+// oo
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        //...
+    }
+}
+
+objeto.processamento()
+
+// PRINCIPIOS IMPORTANTES
+// 1. abstração
+// 2. encapsulamento
+// 3. herança (prototype)
+// 4. polimorfismo
